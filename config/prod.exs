d7abfed30af41381937c5816a72d9b4221668671
Tom Gurion <nagasaki45@gmail.com>
2020-05-04 19:41:21 +0300

    Add the full gigalixir host to prod config to generate correct urls

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :gamixir, GamixirWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "flat-dangerous-woodnymphbutterfly.gigalixirapp.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: ["//localhost", "//*.gigalixirapp.com"]
 
