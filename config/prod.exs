682c46ada028955bd635e57658bbaaa474cc7e0c
Tom Gurion <nagasaki45@gmail.com>
2020-05-04 19:33:06 +0300

    Try to fix websockets in production
    Folling advice from
    https://elixirforum.com/t/websocket-connection-works-on-localhost-but-get-403-error-when-deployed-via-docker/7466

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :gamixir, GamixirWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["//localhost", "//*.gigalixirapp.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
