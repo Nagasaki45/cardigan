2b7a2abecb41e933a4fb4c65942e8d390f04f4eb
Tom Gurion <nagasaki45@gmail.com>
2020-05-06 19:12:25 +0300

    Support cards front and back CSS styling

--- lib/gamixir/card.ex
+++ lib/gamixir/card.ex
@@ -1,9 +1,8 @@
 defmodule Gamixir.Card do
   defstruct id: nil,
             text: nil,
-            text_color: "black",
-            front_background_color: "white",
-            back_background_color: "black",
+            front_style: %{},
+            back_style: %{},
             face: false
 
   def flip(%__MODUE__{face: face} = card), do: Map.put(card, :face, not face)
