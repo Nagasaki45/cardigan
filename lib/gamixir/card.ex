bb68cf9b6d0a2b00e0b1dd0e690e37c91c9688c1
Tom Gurion <nagasaki45@gmail.com>
2020-05-08 16:26:40 +0300

    Tooltip support. Fix #5

--- lib/gamixir/card.ex
+++ lib/gamixir/card.ex
@@ -1,6 +1,7 @@
 defmodule Gamixir.Card do
   defstruct id: nil,
             text: nil,
+            tooltip: nil,
             style: %{},
             front_style: %{},
             back_style: %{},
